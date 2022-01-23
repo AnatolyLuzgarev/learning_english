e3aebf1a3592d9210d92b3355094eab1b2f49df9
Tolik <IngrayTolik@yandex.ru>
2022-01-23 13:32:04 +0300

    deleted some trash

--- other_apps/ftp_server.py
+++
@@ -1,8 +0,0 @@
-from pyftpdlib import servers
-from pyftpdlib.handlers import FTPHandler
-
-
-
-address = ("0.0.0.0", 8003)  # listen on every IP on my machine on port 8003
-server = servers.FTPServer(address, FTPHandler)
-server.serve_forever()
